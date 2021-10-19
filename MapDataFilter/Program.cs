b4a7e5bb82c0b0a93daf70174f1d0222403e9aa7
Brychan Dempsey <brychand@hotmail.com>
2021-10-19 14:59:01 +1300

    Added ParseCoordinates function

--- MapDataFilter/Program.cs
+++ MapDataFilter/Program.cs
@@ -20,6 +20,12 @@ namespace MapDataFilter
                 {
                     try
                     {
+                        // This loop will go through every line of the file (the file path is passed as a command-line argument `args`
+                        // At the moment, it just generates a new sorted list of RoadID: GeometryID, but we can adapt this to better sort our
+                        // data
+
+                        // TODO: Filter out coordinates from part[0]
+
                         string[] parts = line.Split(delimiter);
                         int geoID = int.Parse(parts[1]);
                         int roadID = int.Parse(parts[8]);
@@ -33,7 +39,7 @@ namespace MapDataFilter
                     }
                     catch
                     {
-
+                        
                     }
                     line = sr.ReadLine();
                 }
@@ -47,7 +53,59 @@ namespace MapDataFilter
             }
             int k = 0;
         }
+
+        /// <summary>
+        /// Parses the coordinates from the provided field
+        /// </summary>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public List<Coordinate> ParseCoordinates(string field)
+        {
+            // The format of field might look like so:
+            // MULTILINESTRING ((174.5954606167 -36.0807369167|
+            // 174.594731957296 -36.0796673302854|174.59441926705 -36.0793780918086|
+            // 174.593926779914 -36.0792061121738|174.593473379059 -36.0791982949177|
+            // 174.593090333509 -36.0792217466861|174.592762008751 -36.0791826604055|
+            // 174.592535308324 -36.0789872290022|174.592011552163 -36.0791982949177))
+            // (New-lines manually added)
+
+            // We need to parse these into the Coordinate class, and build a list
+            List<Coordinate> coordinates = new List<Coordinate>();
+            // Before we break the field into coordinate strings, we should get rid of (trim) the unneccessary characters;
+            // there are a few ways to do this, but the easiest is to simply perform a replace for 'MULTILINESTRING', and then trim space and brackets
+            field = field.Replace("MULTILINESTRING", "").Trim(new char[] { '(', ')', ' ' });
+            // This results in field being like so:
+            // 174.594731957296 -36.0796673302854|174.59441926705 -36.0793780918086
+            // We know that each coordinate-pair is delimited by a specific char (I chose the pipe '|' character)
+            string[] coordinatePairs = field.Split('|', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            foreach (string coordinatePair in coordinatePairs)
+            {
+                // Each coordinate half is delimited by space, so split by space
+                string[] parts = coordinatePair.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries); // "StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries" sets both Flags to true, so they are performed
+                                                                                                                                    // If we can parse both numbers, we should set a value
+                if (parts.Length == 2 && // Evaluate the length of parts first! Fail-fast, ensures an exception isn't thrown
+                    double.TryParse(parts[0], out double longitude) && double.TryParse(parts[1], out double latitude))
+                {
+                    Coordinate coordinate = new Coordinate()
+                    {
+                        Latitude = latitude,
+                        Longitude = longitude
+                    };
+                    coordinates.Add(coordinate);
+                }
+                else
+                {
+                    // Set a breakpoint, as this is a failiure to parse
+                    int g = 0;
+                }
+            }
+            return coordinates;
+        }
     }
+    //hELLO!!!
+    
+   
+
 
     struct RoadGeoMap : IComparable
     {
@@ -76,6 +134,11 @@ namespace MapDataFilter
     struct RoadData
     {
         List<Coordinate> RoadPath;
+        // We need two additional fields, at the cost of 16 bytes:
+        // These are where the intersections may be
+        Coordinate StartPoint;
+        Coordinate EndPoint;
+        // Other fields from the source data
         int RoadSectionID;
         string Suburb;
         string City;
